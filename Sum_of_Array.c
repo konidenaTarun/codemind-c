c1685ba5ccdb2c7c916b21c53e895379e6cff043
konidenaTarun <147487727+konidenaTarun@users.noreply.github.com>
2023-12-18 21:48:28 +0530

    #include<stdio.h> int main() { 	int n ,s=0; 	scanf("%d",&n); 	int arr[n],i; 	for(i=0;i<n;i++) 	{ 		scanf("%d",&arr[i]);
    	}
    	for(i=0;i<n;i++)
    	{
    		s=s+arr[i];
    	}
          printf("%d",s);
    }

---
+++ Sum_of_Array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int n ,s=0;
+	scanf("%d",&n);
+	int arr[n],i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+		
+	}
+	for(i=0;i<n;i++)
+	{
+		s=s+arr[i];
+	}
+      printf("%d",s);
+}
+
